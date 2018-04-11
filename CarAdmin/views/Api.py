c87f68789c5f503380141e345e73c0f52d518224
superdun <475098936@qq.com>
2018-04-11 23:11:22 +0800

    'fix'

--- CarAdmin/views/Api.py
+++ CarAdmin/views/Api.py
@@ -76,27 +76,7 @@ def carMonitorApi():
     if 'err' in data:
         return jsonify({'status': 'error', 'msg': data['err'], 'data': []})
     if data['msg'] == 'OK':
-        for i in data['data']:
-            carObj = Gps.query.filter_by(code=i['imei']).first()
-            if carObj and carObj.cars:
-                try:
-                    carId = carObj.cars[0].id
-                    carName = carObj.cars[0].name
-                    carImg = carObj.cars[0].img
-                except:
-                    carId = ""
-                    carName = ""
-                    carImg = ""
-            else:
-                carId = ""
-                carName = ""
-                carImg = ""
-            i['car'] = {'id': carId, 'name': carName, 'img': carImg}
-            address = carApi.address(i['lng'], i['lat'])
-            if address['ret'] == 0:
-                i['address'] = address['address']
-            else:
-                i['address'] = u'获取地理位置失败'
+
         return jsonify({'status': 'ok', 'msg': data['msg'], 'data': data['data']})
     else:
         return jsonify({'status': 'error', 'msg': data['msg'], 'data': []})
