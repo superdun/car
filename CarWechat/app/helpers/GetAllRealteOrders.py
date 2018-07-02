b08ec5271746b2abec7d4592d42eb3f783ea4bb1
superdun <475098936@qq.com>
2018-07-02 21:52:29 +0800

    'fix'

--- CarWechat/app/helpers/GetAllRealteOrders.py
+++ CarWechat/app/helpers/GetAllRealteOrders.py
@@ -9,17 +9,21 @@ def getOrderSumData(sourceOrder,cOrders):
     priceSum = sourceOrder.totalfee
     endDateStr = u"未发车"
     overDate = u"未发车"
+    isOver = ""
     for co in cOrders:
         countSum = countSum+co.count
         priceSum = priceSum+co.totalfee
     if sourceOrder.fromdate:
+        endDate = sourceOrder.fromdate + dt.timedelta(days=countSum)
+        endDateStr = (endDate).strftime('%Y-%m-%d %H:%M:%S')
 
         if sourceOrder.todate:
-            endDate = sourceOrder.fromdate + dt.timedelta(days=countSum)
-            endDateStr = (endDate).strftime('%Y-%m-%d %H:%M:%S')
-            overDate = (endDate - sourceOrder.todate).seconds / 60
+            actDate = sourceOrder.todate
         else:
-            endDate = sourceOrder.fromdate + dt.timedelta(days=countSum)
-            endDateStr = (endDate).strftime('%Y-%m-%d %H:%M:%S')
-            overDate = (endDate - datetime.now()).seconds / 60
-    return {"priceSum":priceSum,"countSum":countSum,"endDate":endDateStr,"overDate":overDate}
+            actDate = datetime.now()
+        overDate = (endDate - actDate).seconds / 60
+        if endDate>actDate:
+            isOver = "-"
+        else:
+            isOver = "+"
+    return {"priceSum":priceSum,"countSum":countSum,"endDate":endDateStr,"overDate":overDate,"isOver":isOver}
